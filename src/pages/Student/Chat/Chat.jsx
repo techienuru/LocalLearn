ae7cfc141bc2714a43748de1278a96d2d696359d
Ibrahim Nurudeen shehu <ibrahimnurudeenshehu1447@gmail.com>
2025-12-12 20:41:45 +0100

    feat: enhance StudentChat component with Bell icon and improved navigation layout

--- src/pages/Student/Chat/Chat.jsx
+++ src/pages/Student/Chat/Chat.jsx
@@ -1,4 +1,4 @@
-import { Diamond } from "lucide-react";
+import { BellIcon, Diamond } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const StudentChat = () => {
@@ -6,23 +6,25 @@ const StudentChat = () => {
     <>
       {/* Top Navigation Bar */}
       <header>
-        <nav className="">
-          <div>
+        <nav className="bg-amber-300 justify-between flex px-5 py-3">
+          <div className="flex gap-3 items-center">
             <div className="size-6 text-primary">
               <Diamond />
             </div>
-            <h2 className=" text-lg font-bold leading-tight">LocalLearn</h2>
+            <h2 className=" text-lg font-bold">LocalLearn</h2>
           </div>
-          <div className="flex gap-1.5">
-            <i>Bell icon</i>
-            <img src="" alt="profile img" />
+          <div className="flex gap-2 items-center">
+            <span className="bg-gray-200 py-2 px-2 rounded-md text-gray-600">
+              <BellIcon />
+            </span>
+            <img src="./vite.svg" alt="profile img" className="rounded-full" />
           </div>
         </nav>
       </header>
 
-      <main>
+      <main className="flex bg-amber-700">
         {/* Side Navigation Bar */}
-        <aside className="flex h-100 flex-col justify-between">
+        <aside className="flex flex-[15%] h-100 flex-col justify-between bg-black">
           <ul className="flex flex-col gap-4">
             <li className="">
               <i>Home</i>
@@ -32,7 +34,7 @@ const StudentChat = () => {
           <ul className="flex flex-col gap-2"></ul>
         </aside>
         {/* Inner section */}
-        {/* <section className=""></section> */}
+        <section className="flex-[85%]"></section>
       </main>
     </>
   );
